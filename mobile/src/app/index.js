c1dabf67f47015b187dbbf5be77a93f6c01e11a2
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-02-10 11:31:08 -0500

    camera wip

---
+++ mobile/src/app/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+import UploadScreen from "../screens/UploadScreen";
+
+const Main = () => {
+  return <UploadScreen />;
+};
+
+export default Main;
