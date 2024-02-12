d62e2cfc6f5ef4e08db35a675847bc97aa3171fb
Hugo Liu <98724522+hugoliu-code@users.noreply.github.com>
2024-02-11 21:35:36 -0500

    Final Changes

--- frontend/src/components/AnimalCard.js
+++ frontend/src/components/AnimalCard.js
@@ -5,12 +5,12 @@ function AnimalCard(props) {
     const animal_img = props.animal_img;
     return(    
     <>
-        <div className="card" style={{"width": "1000px", "marginLeft": "10px", "marginRight" : "10px", "marginTop" : "10px","float":"left"}}>
-            <img src={"data:image/jpeg;base64,"+ animal_img} alt="Base64 Image" className="card-img-top"/>
-            <div className="card-body" style={{"background": "	#baffc9", "color": "#ffb3ba"}}>
-                <h5 className="card-title">{animal_class}</h5>
+        <div className="card" style={{"width": "300px", "marginLeft": "10px", "marginRight" : "10px", "marginTop" : "10px","float":"left", padding:"30px", backgroundColor:"#ffffba", borderRadius:"20px"}}>
+            <img src={"data:image/jpeg;base64,"+ animal_img} alt="Base64 Image" className="card-img-top" style={{marginBottom:"10px"}}/>
+            <div className="card-body" style={{"color": "black"}}>
+                <h5 className="card-title">Type: {animal_class}</h5>
                 <h5 className="card-title" style={{"fontSize": "1em"}}>{props.animal_time && "Time Spotted: " + props.animal_time}</h5>
-                <h5 className="card-title" style={{"fontSize": "1em"}}>{props.animal_location && "Location: " + props.animal_location}</h5>
+                <h5 className="card-title" style={{"fontSize": "1em"}}>{props.animal_location && "Location Spotted: " + props.animal_location}</h5>
             </div>
         </div>
     </>
--- frontend/src/components/AnimalGroup.js
+++ frontend/src/components/AnimalGroup.js
@@ -1,22 +1,22 @@
-
 import AnimalCard from "./AnimalCard";
 import './AnimalGroup.css';
 // {items:[], heading: string}
 
 
 function AnimalGroup(props) {
-
-    return(    
-    <>
-        <div class="container">
-            {props.items.map(item => <AnimalCard animal_class = {item["class"]} 
-            animal_img = {item["image"]} 
-            animal_location = {item["location"]} 
-            animal_time = {item["time"]}
-            key = {item["_id"]}></AnimalCard>)}
-            </div>
-    </>
-    )
+    return (
+        <div className="animal-group">
+            {props.items.map(item => (
+                <AnimalCard 
+                    animal_class={item["class"]} 
+                    animal_img={item["image"]} 
+                    animal_location={item["location"]} 
+                    animal_time={item["time"]}
+                    key={item["_id"]} 
+                />
+            ))}
+        </div>
+    );
 }
 
 export default AnimalGroup;
